bb1c1e3a00aa58f080d75aa2c0923f624955e291
Luiz Henrique Miranda de Assis <luiz.miranda.ti@hotmail.com>
2019-08-24 15:35:57 -0300

    Adicionando pesquisa na listagem de séries

--- src/pages/series/list.js
+++ src/pages/series/list.js
@@ -1,24 +1,36 @@
 import React, {Component} from 'react'
 import Gateway from '../../lib/axios'
-import { Table } from 'react-materialize';
+import { Table, TextInput } from 'react-materialize';
 
 class List extends Component {
+
+    //# (validate) Utilizar função retornando (true/false)
+    //# (validate) Realizar validação mínimo 3 caracteres
+    //# (filter)   tratamento não encontrado na listagem
+    //# (Pull Request) -- Pontuação maior
+    //# (Ou enviar arquivo por e-mail) -- Pontuação menor
+
 
     constructor () {
       super()
 
       this.state = {
         isLoading : true,
-        data: {}
+        source: {},
+        data: {},
+        filter: '',
+        classValidate: 'validate valid',
+        msgError: ''
       }
     }
 
    async componentDidMount() {
-        const response = await Gateway.request("http://10.68.102.35:4000/series")
+        const response = await Gateway.request("http://10.68.102.181:4000/series")
 
         this.setState({
             isLoading :false,
-            data : response.data
+            data : response.data,
+            source: response.data
         });
     }
 
@@ -48,7 +60,12 @@ class List extends Component {
          return (
            <div>
             <h1>Lista de séries</h1>
-               <Table>
+            
+            <div>
+               <TextInput className={this.state.classValidate} error={this.state.msgError} value={this.state.filter} placeholder="Pesquisar" onChange={(e)=> this.change(e)} />
+            </div>
+
+            <Table>
                     <thead>
                         <tr>
                             <th data-field="id">
@@ -74,9 +91,53 @@ class List extends Component {
        
     }
 
+    change(event) {
+       const value = event.target.value
+     
+       this.setState({
+         filter:value
+       })
+
+       this.filter(value)
+    }
+
+    filter(value) {
+        const {source} = this.state
+
+        this.validate(value)
+
+        const found = (value.trim() === '')
+        ? source
+        : source.filter(function(serie){
+            return serie.descricao.toLowerCase().indexOf(value.toLowerCase()) !== -1
+        })
+
+        this.setState({
+          data: found
+       })
+    }
+
+    validate(value) {
+
+      const success = "validate valid"
+      const error = "validate invalid"
+      let msgError = ""
+      let classValidate = "" 
+
+      if(value.trim() === '')
+      {
+        msgError = "Campo obrigatório"
+        classValidate = error;
+      }
+
+      this.setState({
+          classValidate,
+          msgError
+      })
+    }
+
     render() {
-        return  this.renderHtml()
-        
+        return  this.renderHtml()     
     }
 }
 
